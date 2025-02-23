4b55144f7e6290b65b519bbfccc15514509b8514
rytst <r.saito0111@gmail.com>
2025-02-23 18:53:17 +0900

    :sparkles: Encoder

--- the-annotated-transformer/transformer.py
+++ the-annotated-transformer/transformer.py
@@ -1,9 +1,15 @@
+import copy
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+##################
+# Model Overview #
+##################
+
 class EncoderDecoder(nn.Module):
-    def __init__(self, encoder, decoder, src_embed, tgt_embed, generator):
+    def __init__(self, encoder, decoder, src_embed, tgt_embed, generator) -> None:
         super().__init__()
         self.encoder = encoder
         self.decoder = decoder
@@ -19,7 +25,7 @@ class EncoderDecoder(nn.Module):
 
     def decode(self, memory, src_mask, tgt, tgt_mask):
         return self.decoder(
-            self.src_embed(tgt),
+            self.tgt_embed(tgt),
             memory,
             src_mask,
             tgt_mask
@@ -34,9 +40,72 @@ class EncoderDecoder(nn.Module):
         )
 
 class Generator(nn.Module):
-    def __init__(self, d_model, vocab):
+    def __init__(self, d_model, vocab) -> None:
         super().__init__()
         self.proj = nn.Linear(d_model, vocab)
 
     def forward(self, x):
         return F.log_softmax(self.proj(x), dim=-1)
+
+
+###########
+# Encoder #
+###########
+
+def clones(module, N: int):
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+
+class Encoder(nn.Module):
+    def __init__(self, layer, N: int) -> None:
+        super().__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+
+        return self.norm(x)
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, features, eps: float =1e-6) -> None:
+        super().__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps: float = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+
+class SublayerConnection(nn.Module):
+    def __init__(self, size, dropout: float) -> None:
+        super().__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, size, self_attn, feed_forward, dropout: float) -> None:
+        super().__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        self.size = size
+
+    def forward(self, x, mask):
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
+        return self.sublayer[1](x, self.feed_forward)
+
+
+###########
+# Decoder #
+###########

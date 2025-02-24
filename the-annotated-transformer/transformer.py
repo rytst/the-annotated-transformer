4a62d203d8e5b469c1f3bb1b2943686cd84b6558
rytst <r.saito0111@gmail.com>
2025-02-25 01:46:49 +0900

    :construction: WIP decoder

--- the-annotated-transformer/transformer.py
+++ the-annotated-transformer/transformer.py
@@ -47,14 +47,26 @@ class Generator(nn.Module):
     def forward(self, x):
         return F.log_softmax(self.proj(x), dim=-1)
 
+###################
+# Helper Function #
+###################
+
+# The encoder and decoder are respectively composed of
+# a stack of N = 6 identical layers
+def clones(module, N: int):
+    # A shallow copy constructs a new compound object
+    # and then (to the extent possible) inserts references into it
+    # to the objects found in the original.
+    #
+    # A deep copy constructs a new compound object
+    # and then, recursively, inserts copies into it of the objects
+    # found in the original.
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
 
 ###########
 # Encoder #
 ###########
-
-def clones(module, N: int):
-    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
-
 
 class Encoder(nn.Module):
     # N = 6 in `Attention Is All You Need`
@@ -113,3 +125,46 @@ class EncoderLayer(nn.Module):
 ###########
 # Decoder #
 ###########
+
+class Decoder(nn.Module):
+    def __init__(self, layer, N: int) -> None:
+        super().__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, memory, src_mask, tgt_mask)
+
+        return self.norm(x)
+
+class DecoderLayer(nn.Module):
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout: float) -> None:
+        super().__init__()
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 3)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        m = memory
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
+        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
+        return self.sublayer[2](x, self.feed_forward)
+
+def subsequent_mask(size: int):
+    attn_shape = (1, size, size)
+
+    subsequent_mask = torch.triu( # Returns the uppper triangular
+        torch.ones(attn_shape),   # part of a matrix
+        diagonal=1
+    ).type(
+        torch.uint8
+    )
+
+    return subsequent_mask == 0
+
+
+
+

8c41c9dab9c3192ff8e5e6fd6fdfa50a7af026d0
rytst <r.saito0111@gmail.com>
2025-02-25 17:01:22 +0900

    :sparkles: Susequent masking

--- test/test_transformer.py
+++ test/test_transformer.py
@@ -1,8 +1,35 @@
 import unittest
+import altair as alt
+import polars as pl
 import transformer as t
 
 def example_mask() -> None:
-    print("hello")
+    LS_data = pl.concat(
+        [
+            pl.DataFrame(
+                {
+                    "Subsequent Mask": t.subsequent_mask(20)[0][x, y],
+                    "Window": y,
+                    "Masking": x,
+                }
+            )
+            for y in range(20)
+            for x in range(20)
+        ]
+    )
+
+    chart = (
+        alt.Chart(LS_data)
+        .mark_rect()
+        .properties(height=250, width=250)
+        .encode(
+            alt.X("Window:O"),
+            alt.Y("Masking:O"),
+            alt.Color("Subsequent Mask:Q", scale=alt.Scale(scheme="viridis")),
+        )
+    )
+
+    chart.save("img/mask.png")
 
 
 class TestSubsequentMask(unittest.TestCase):
